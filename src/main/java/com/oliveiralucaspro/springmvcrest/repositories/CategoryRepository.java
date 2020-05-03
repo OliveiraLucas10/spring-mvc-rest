bc93baddb9c181e064b56e37fd52d14ef88e071b
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-03 13:10:55 +0100

    first entety and dto (category)

---
+++ src/main/java/com/oliveiralucaspro/springmvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.oliveiralucaspro.springmvcrest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oliveiralucaspro.springmvcrest.domain.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
