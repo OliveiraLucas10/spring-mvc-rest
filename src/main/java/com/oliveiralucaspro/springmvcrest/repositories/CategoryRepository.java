3f5c4b29ae8ecd1be7bd72000d08a733f637ba82
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-03 14:45:41 +0100

    added category: Service, Repository and Test

--- src/main/java/com/oliveiralucaspro/springmvcrest/repositories/CategoryRepository.java
+++ src/main/java/com/oliveiralucaspro/springmvcrest/repositories/CategoryRepository.java
@@ -6,4 +6,6 @@ import com.oliveiralucaspro.springmvcrest.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Category findByName(String name);
+
 }
