32aaf9d9f820194580b8fdc0a838d98e94db3be9
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-04 06:47:06 +0100

    added data to bootstrap

--- src/main/java/com/oliveiralucaspro/springmvcrest/bootstrap/Bootstrap.java
+++ src/main/java/com/oliveiralucaspro/springmvcrest/bootstrap/Bootstrap.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.oliveiralucaspro.springmvcrest.domain.Category;
+import com.oliveiralucaspro.springmvcrest.domain.Customer;
 import com.oliveiralucaspro.springmvcrest.repositories.CategoryRepository;
+import com.oliveiralucaspro.springmvcrest.repositories.CustomerRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +15,15 @@ import lombok.RequiredArgsConstructor;
 public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
+    private final CustomerRepository customerRepository;
 
     @Override
     public void run(String... args) throws Exception {
+	loadCategory();
+	loadCustomer();
+    }
+
+    private void loadCategory() {
 	Category fruits = new Category();
 	fruits.setName("Fruits");
 
@@ -37,7 +45,21 @@ public class Bootstrap implements CommandLineRunner {
 	categoryRepository.save(exotic);
 	categoryRepository.save(nuts);
 
-	System.out.println("Data Loaded = " + categoryRepository.count());
+	System.out.println("Category Loaded = " + categoryRepository.count());
+    }
 
+    private void loadCustomer() {
+	Customer pedro = new Customer();
+	pedro.setFirstName("Pedro");
+	pedro.setLastName("Favela");
+
+	Customer sergio = new Customer();
+	sergio.setFirstName("Sergio");
+	sergio.setLastName("Mariola");
+
+	customerRepository.save(pedro);
+	customerRepository.save(sergio);
+
+	System.out.println("Customer Loaded = " + customerRepository.count());
     }
 }
