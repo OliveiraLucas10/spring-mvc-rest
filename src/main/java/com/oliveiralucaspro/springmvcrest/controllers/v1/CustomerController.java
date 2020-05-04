48c026878c6fc9dc5175338290923cee46cb486b
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-04 07:12:35 +0100

    added Customer list, controller and test

---
+++ src/main/java/com/oliveiralucaspro/springmvcrest/controllers/v1/CustomerController.java
@@ -0,0 +1,32 @@
+package com.oliveiralucaspro.springmvcrest.controllers.v1;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.oliveiralucaspro.springmvcrest.api.v1.model.CustomerDTO;
+import com.oliveiralucaspro.springmvcrest.api.v1.model.CustomerListDTO;
+import com.oliveiralucaspro.springmvcrest.services.CustomerService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/api/v1/customers/")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<CustomerListDTO> getAllCustomers() {
+	return new ResponseEntity<>(new CustomerListDTO(customerService.getAllCustomers()), HttpStatus.OK);
+    }
+
+    @GetMapping("{firstName}")
+    public ResponseEntity<CustomerDTO> getCustomerByFirstName(@PathVariable String firstName) {
+	return new ResponseEntity<>(customerService.getCustomerByFirstName(firstName), HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/oliveiralucaspro/springmvcrest/controllers/v1/CustomerControllerTest.java
@@ -0,0 +1,84 @@
+package com.oliveiralucaspro.springmvcrest.controllers.v1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.oliveiralucaspro.springmvcrest.api.v1.model.CustomerDTO;
+import com.oliveiralucaspro.springmvcrest.api.v1.model.CustomerListDTO;
+import com.oliveiralucaspro.springmvcrest.services.CustomerService;
+
+class CustomerControllerTest {
+
+    private static final String LAST_NAME = "Favela";
+
+    private static final String FIRST_NAME = "Pedro";
+
+    private static final Long ID = 1L;
+
+    @Mock
+    CustomerService customerService;
+
+    @InjectMocks
+    CustomerController customerController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() throws Exception {
+	MockitoAnnotations.initMocks(this);
+
+	mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    }
+
+    @Test
+    void testGetAllCustomers() throws Exception {
+	// given
+	CustomerDTO customerDTO1 = new CustomerDTO();
+	customerDTO1.setId(ID);
+	customerDTO1.setFirstName(FIRST_NAME);
+	customerDTO1.setLastName(LAST_NAME);
+
+	CustomerDTO customerDTO2 = new CustomerDTO();
+	customerDTO2.setId(2L);
+	customerDTO2.setFirstName("Serio");
+	customerDTO2.setLastName("Mariola");
+
+	List<CustomerDTO> customerDTOList = Arrays.asList(customerDTO1, customerDTO2);
+	when(customerService.getAllCustomers()).thenReturn(customerDTOList);
+
+	mockMvc.perform(get("/api/v1/customers/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.customers", hasSize(2)));
+    }
+    
+    @Test
+    void testGetCustomerByFirstName() throws Exception {
+	// given
+	CustomerDTO customerDTO1 = new CustomerDTO();
+	customerDTO1.setId(ID);
+	customerDTO1.setFirstName(FIRST_NAME);
+	customerDTO1.setLastName(LAST_NAME);
+	
+	when(customerService.getCustomerByFirstName(anyString())).thenReturn(customerDTO1);
+	
+	mockMvc.perform(get("/api/v1/customers/Pedro").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+	.andExpect(jsonPath("$.firstName", equalTo(FIRST_NAME)));
+    }
+
+}
