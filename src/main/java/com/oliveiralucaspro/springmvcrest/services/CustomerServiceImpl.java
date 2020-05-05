ebf10fd922288dcab908ddffecfcc41f7343969c
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-05 06:05:01 +0100

    added patch for customer

--- src/main/java/com/oliveiralucaspro/springmvcrest/services/CustomerServiceImpl.java
+++ src/main/java/com/oliveiralucaspro/springmvcrest/services/CustomerServiceImpl.java
@@ -1,7 +1,6 @@
 package com.oliveiralucaspro.springmvcrest.services;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -54,6 +53,27 @@ public class CustomerServiceImpl implements CustomerService {
 	CustomerDTO returnCustomerDTO = customerMapper.customerToCustomerDTO(savedCustomer);
 	returnCustomerDTO.setCustomerUrl(ROOT_URL + savedCustomer.getId());
 	return returnCustomerDTO;
+    }
+
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+	return customerRepository.findById(id).map(customer -> {
+
+	    if (customerDTO.getFirstName() != null) {
+		customer.setFirstName(customerDTO.getFirstName());
+	    }
+
+	    if (customerDTO.getLastName() != null) {
+		customer.setLastName(customerDTO.getLastName());
+	    }
+
+	    CustomerDTO returnDto = customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+
+	    returnDto.setCustomerUrl(ROOT_URL + id);
+
+	    return returnDto;
+
+	}).orElseThrow(RuntimeException::new); // todo implement better exception handling;
     }
 
 }
--- src/test/java/com/oliveiralucaspro/springmvcrest/controllers/v1/CustomerControllerTest.java
+++ src/test/java/com/oliveiralucaspro/springmvcrest/controllers/v1/CustomerControllerTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -118,6 +119,29 @@ class CustomerControllerTest extends AbstractRestControllerTest{
 
         //when/then
         mockMvc.perform(put("/api/v1/customers/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(customer)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", equalTo(FIRST_NAME)))
+                .andExpect(jsonPath("$.lastName", equalTo(LAST_NAME)))
+                .andExpect(jsonPath("$.customer_url", equalTo(CUSTOMER_URL_1)));
+    }
+    
+    @Test
+     void testPatchCustomer() throws Exception {
+
+        //given
+        CustomerDTO customer = new CustomerDTO();
+        customer.setFirstName(FIRST_NAME);
+
+        CustomerDTO returnDTO = new CustomerDTO();
+        returnDTO.setFirstName(customer.getFirstName());
+        returnDTO.setLastName(LAST_NAME);
+        returnDTO.setCustomerUrl(CUSTOMER_URL_1);
+
+        when(customerService.patchCustomer(anyLong(), any(CustomerDTO.class))).thenReturn(returnDTO);
+
+        mockMvc.perform(patch("/api/v1/customers/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(customer)))
                 .andExpect(status().isOk())
